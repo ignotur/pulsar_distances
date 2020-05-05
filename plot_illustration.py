e32c7aafc89997cdd8ca853ccedee0f2590d30bb
Dr Andrei Igoshev <ignotur@tests-MacBook-Pro.local>
2020-05-05 11:41:39 +0100

    Enchanced comments

--- plot_illustration.py
+++ plot_illustration.py
@@ -9,20 +9,29 @@ mpl.rcParams.update({'font.size': 12})
 mpl.rcParams.update({'legend.labelspacing':0.25, 'legend.fontsize': 12})
 mpl.rcParams.update({'errorbar.capsize': 4})
 
+### Program to estimate the posterior distance given parallax
+### This program can be found at https://github.com/ignotur/pulsar_distances 
+### Written by Andrei Igoshev (ignotur@gmail.com)
 
+
+## Linking the C++ library
 lib = cdll.LoadLibrary('./d_post.so')
 lib.post_d.restype = ctypes.c_double
 lib.cnd_varpi.restype = ctypes.c_double
 lib.int_d.restype = ctypes.c_double
 
+###########################################
 ## Example for PSR J0218+4232
+## Enter here measurements for radio pulsar
 
-gl = 139.51
+gl = 139.51         ## Galactic longitude and latitude
 gb = -17.53
-varpi     = 0.16    ## mas
-varpi_err = 0.09
+varpi     = 0.16    ## parallax [mas]
+varpi_err = 0.09    ## parallax error [mas]
 
-d = np.linspace (0.01, 10, 100) ## range of possible distances
+###########################################
+
+d = np.linspace (0.01, 10, 100) ## range of distances
 post_d = []
 
 cnd_p   = []
@@ -36,8 +45,8 @@ for i in range (0, len(d)):
 	varpi_     = c_double (varpi)
 	varpi_err_ = c_double (varpi_err)
 
-	post_d_res = lib.post_d (di, varpi_, varpi_err_, l, b)
-	post_d.append (post_d_res)
+	post_d_res = lib.post_d (di, varpi_, varpi_err_, l, b) ## This function returns the posterior
+	post_d.append (post_d_res)                             ## This list will contain the values of the posterior for each distance
 
 	cnd_v = lib.cnd_varpi (varpi_, varpi_err_, di)
 
@@ -49,6 +58,8 @@ for i in range (0, len(d)):
 
 	cnd_p.append   (cnd_v)
 	prior_p.append (prior_v)
+
+## Here I compute some useful statistics such as the location of the postrior maximum and credible range.
 
 print ('Maximum of the posterior is at: ', round(d[np.argmax(post_d)], 2), ' kpc')
 cum_post = np.cumsum(post_d) / np.sum(post_d)
@@ -75,6 +86,7 @@ plt.plot (d, post_d / np.max(post_d), 'k-', label=r'$P_D (D | \varpi)$')
 plt.legend()
 plt.xlabel ('D (kpc)')
 plt.ylabel ('Relative probability')
+plt.savefig('illustration.png')
 plt.show()
 
 
--- plot_posterior.py
+++ plot_posterior.py
@@ -9,16 +9,23 @@ mpl.rcParams.update({'font.size': 12})
 mpl.rcParams.update({'legend.labelspacing':0.25, 'legend.fontsize': 12})
 mpl.rcParams.update({'errorbar.capsize': 4})
 
+### Program to estimate the posterior distance given parallax
+### This program can be found at https://github.com/ignotur/pulsar_distances 
+### Written by Andrei Igoshev (ignotur@gmail.com)
 
 lib = cdll.LoadLibrary('./d_post.so')
 lib.post_d.restype = ctypes.c_double
 
+###########################################
 ## Example for PSR J0218+4232
+## Enter here measurements for radio pulsar
 
-gl = 139.51
+gl = 139.51         ## Galactic longitude and latitude
 gb = -17.53
-varpi     = 0.16    ## mas
-varpi_err = 0.09
+varpi     = 0.16    ## parallax [mas]
+varpi_err = 0.09    ## parallax error [mas]
+
+###########################################
 
 d = np.linspace (0.01, 10, 100) ## range of possible distances
 post_d = []
@@ -32,7 +39,10 @@ for i in range (0, len(d)):
 	varpi_err_ = c_double (varpi_err)
 
 	post_d_res = lib.post_d (di, varpi_, varpi_err_, l, b)
-	post_d.append (post_d_res)
+	post_d.append (post_d_res) ## This list will contain the values of the posterior for each distance
+
+
+## Here I compute some useful statistics such as the location of the postrior maximum and credible range.
 
 print ('Maximum of the posterior is at: ', round(d[np.argmax(post_d)], 2), ' kpc')
 cum_post = np.cumsum(post_d) / np.sum(post_d)
